4410e337205cd7869169622fd26404fc4f282d68
akolomytsev <akolomytsev@ptsecurity.com>
2024-03-06 14:59:34 +0300

    add soap fixture and tests with soap asserts

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -7,5 +7,15 @@ def test_add_project(app, login, db, json_project):
     app.project.create(project)
     new_project_list = db.get_project_list()
     old_project_list.append(project)
-    assert sorted(old_project_list, key=operator.attrgetter('name')) == sorted(new_project_list,
-                                                                               key=operator.attrgetter('name'))
+    assert sorted(old_project_list, key=operator.attrgetter('name')) == \
+           sorted(new_project_list, key=operator.attrgetter('name'))
+
+
+# def test_add_project_soap(app, login, json_project):
+#     project = json_project
+#     old_project_list = app.soap.get_projects()
+#     app.project.create(project)
+#     new_project_list = app.soap.get_projects()
+#     old_project_list.append(project)
+#     assert sorted(old_project_list, key=operator.attrgetter('name')) == \
+#            sorted(new_project_list, key=operator.attrgetter('name'))
